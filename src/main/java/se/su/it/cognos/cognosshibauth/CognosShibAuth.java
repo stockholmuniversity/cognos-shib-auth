0333c2425f95712226cb1664aea8d3fcae9bf33b
Joakim Lundin <jolu@it.su.se>
2011-04-04 13:05:04 +0200

    Added base class for authenticator.

---
+++ src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuth.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: joakim
+ * Date: 2011-04-04
+ * Time: 12:54
+ * To change this template use File | Settings | File Templates.
+ */
+package se.su.it.cognos.cognosshibauth;
+
+public class CognosShibAuth {
+}
---
+++ src/test/java/se/su/it/cognos/cognosshibauth/CognosShibAuthTest.java
@@ -0,0 +1,11 @@
+package se.su.it.cognos.cognosshibauth;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joakim
+ * Date: 2011-04-04
+ * Time: 13:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class CognosShibAuthTest {
+}
