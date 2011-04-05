f180f535e8d1d676a9747b6bd10613c630a88b26
Joakim Lundin <jolu@it.su.se>
2011-04-05 11:38:52 +0200

    Indentation

--- src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuth.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuth.java
@@ -18,63 +18,63 @@ import java.util.Locale;
 public class CognosShibAuth implements INamespaceAuthenticationProvider2 {
 
 
-    public IVisa logon(IBiBusHeader2 iBiBusHeader2) throws UserRecoverableException, SystemRecoverableException, UnrecoverableException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public IVisa logon(IBiBusHeader2 iBiBusHeader2) throws UserRecoverableException, SystemRecoverableException, UnrecoverableException {
+    return null;
+  }
 
-    public void logoff(IVisa iVisa, IBiBusHeader iBiBusHeader) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public void logoff(IVisa iVisa, IBiBusHeader iBiBusHeader) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public IQueryResult search(IVisa iVisa, IQuery iQuery) throws UnrecoverableException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public IQueryResult search(IVisa iVisa, IQuery iQuery) throws UnrecoverableException {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public void init(INamespaceConfiguration iNamespaceConfiguration) throws UnrecoverableException {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public void init(INamespaceConfiguration iNamespaceConfiguration) throws UnrecoverableException {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public void destroy() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public void destroy() {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public String getNamespaceFormat() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public String getNamespaceFormat() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public void setNamespaceFormat(String s) throws UnrecoverableException {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public void setNamespaceFormat(String s) throws UnrecoverableException {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public String[] getCapabilities() {
-        return new String[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public String[] getCapabilities() {
+    return new String[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public String getDescription(Locale locale) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public String getDescription(Locale locale) {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public Locale[] getAvailableDescriptionLocales() {
-        return new Locale[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public Locale[] getAvailableDescriptionLocales() {
+    return new Locale[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public IBaseClass[] getAncestors() {
-        return new IBaseClass[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public IBaseClass[] getAncestors() {
+    return new IBaseClass[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public boolean getHasChildren() {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public boolean getHasChildren() {
+    return false;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public String getName(Locale locale) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public String getName(Locale locale) {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public Locale[] getAvailableNameLocales() {
-        return new Locale[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public Locale[] getAvailableNameLocales() {
+    return new Locale[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public String getObjectID() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public String getObjectID() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 }
--- src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuthVisa.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuthVisa.java
@@ -12,27 +12,27 @@ import com.cognos.CAM_AAA.authentication.*;
 
 public class CognosShibAuthVisa implements IVisa {
 
-    public ITrustedCredential generateTrustedCredential(IBiBusHeader iBiBusHeader) throws UserRecoverableException, SystemRecoverableException, UnrecoverableException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public ITrustedCredential generateTrustedCredential(IBiBusHeader iBiBusHeader) throws UserRecoverableException, SystemRecoverableException, UnrecoverableException {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public ICredential generateCredential(IBiBusHeader iBiBusHeader) throws UserRecoverableException, SystemRecoverableException, UnrecoverableException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public ICredential generateCredential(IBiBusHeader iBiBusHeader) throws UserRecoverableException, SystemRecoverableException, UnrecoverableException {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public boolean isValid() {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public boolean isValid() {
+    return false;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public IAccount getAccount() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public IAccount getAccount() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public IGroup[] getGroups() {
-        return new IGroup[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public IGroup[] getGroups() {
+    return new IGroup[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
 
-    public IRole[] getRoles() {
-        return new IRole[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
+  public IRole[] getRoles() {
+    return new IRole[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
 }
