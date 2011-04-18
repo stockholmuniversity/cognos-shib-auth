8d04f625743c92f74af26cd2c80e59bf128f0b50
Joakim Lundin <jolu@it.su.se>
2011-04-18 07:19:19 +0200

    Added test for capabilities.

--- src/test/java/se/su/it/cognos/cognosshibauth/config/ConfigHandlerTest.java
+++ src/test/java/se/su/it/cognos/cognosshibauth/config/ConfigHandlerTest.java
@@ -6,12 +6,14 @@ package se.su.it.cognos.cognosshibauth.config;
  * Time: 10:48
  */
 
+import com.cognos.CAM_AAA.authentication.INamespace;
 import com.cognos.CAM_AAA.authentication.INamespaceConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 import se.su.it.cognos.cognosshibauth.CognosShibAuth;
 
+import java.util.List;
 import java.util.Locale;
 
 import static org.mockito.Mockito.*;
@@ -28,6 +30,20 @@ public class ConfigHandlerTest {
     String sv_name = configHandler.getName(locale_sv);
 
     assertEquals("Swedish Name", sv_name);
+  }
+
+  @Test
+  public void testThatGetCapabilitiesReturnsAllCapabilities() throws Exception {
+    ConfigHandler configHandler = new ConfigHandler();
+
+    List<String> list = configHandler.getCapabilities();
+
+    assertTrue(list.contains(INamespace.CapabilityCaseSensitive));
+    assertTrue(list.contains(INamespace.CapabilityContains));
+    assertTrue(list.contains(INamespace.CapabilityEndsWith));
+    assertTrue(list.contains(INamespace.CapabilityEquals));
+    assertTrue(list.contains(INamespace.CapabilitySort));
+    assertTrue(list.contains(INamespace.CapabilityStartsWith));
   }
 }
 
