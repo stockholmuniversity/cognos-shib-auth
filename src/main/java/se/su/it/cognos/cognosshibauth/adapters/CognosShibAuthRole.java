4ea0cd05c83428fd12d5a10ca44997dee9db719b
Joakim Lundin <jolu@it.su.se>
2011-04-19 08:45:55 +0200

    Switched from vector to list and did some indenting

--- src/main/java/se/su/it/cognos/cognosshibauth/adapters/CognosShibAuthRole.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/adapters/CognosShibAuthRole.java
@@ -1,40 +1,25 @@
-
 package se.su.it.cognos.cognosshibauth.adapters;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 import com.cognos.CAM_AAA.authentication.IBaseClass;
 import com.cognos.CAM_AAA.authentication.IRole;
 
+public class CognosShibAuthRole extends CognosShibAuthUiClass implements IRole {
 
-public class CognosShibAuthRole extends CognosShibAuthUiClass implements IRole
-{
+  private List<IBaseClass> members = null;
 
-  private Vector	members;
-
-  public CognosShibAuthRole(String theSearchPath)
-  {
+  public CognosShibAuthRole(String theSearchPath) {
     super(theSearchPath);
-    members = null;
+    members = new ArrayList<IBaseClass>();
   }
 
-  public void addMember(IBaseClass theMember)
-  {
-    if (members == null)
-    {
-      members = new Vector();
-    }
+  public void addMember(IBaseClass theMember) {
     members.add(theMember);
   }
 
-  public IBaseClass[] getMembers()
-  {
-    if (members != null)
-    {
-      IBaseClass[] array = new IBaseClass[members.size()];
-      return (IBaseClass[]) members.toArray(array);
-    }
-    return null;
+  public IBaseClass[] getMembers() {
+    return members.toArray(new IBaseClass[members.size()]);
   }
-
-
 }
