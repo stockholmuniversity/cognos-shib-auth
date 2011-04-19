89f653ba847f2773cc27f74f9b9fe424c55998fc
Joakim Lundin <jolu@it.su.se>
2011-04-19 08:50:00 +0200

    Switche from Vector to List and did some refactoring.

--- src/main/java/se/su/it/cognos/cognosshibauth/adapters/CognosShibAuthGroup.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/adapters/CognosShibAuthGroup.java
@@ -1,40 +1,24 @@
-
 package se.su.it.cognos.cognosshibauth.adapters;
-import java.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cognos.CAM_AAA.authentication.IBaseClass;
 import com.cognos.CAM_AAA.authentication.IGroup;
 
+public class CognosShibAuthGroup extends CognosShibAuthUiClass implements IGroup {
+  private List<IBaseClass> members = null;
 
-public class CognosShibAuthGroup extends CognosShibAuthUiClass implements IGroup
-{
-  private Vector	members;
-
-  public CognosShibAuthGroup(String theObjectID)
-  {
+  public CognosShibAuthGroup(String theObjectID) {
     super(theObjectID);
-    members = null;
+    members = new ArrayList<IBaseClass>();
   }
 
-  public void addMember(IBaseClass theMember)
-  {
-    if (members == null)
-    {
-      members = new Vector();
-    }
+  public void addMember(IBaseClass theMember) {
     members.add(theMember);
   }
 
-
-  public IBaseClass[] getMembers()
-  {
-    if (members != null)
-    {
-      IBaseClass[] array = new IBaseClass[members.size()];
-      return (IBaseClass[]) members.toArray(array);
-    }
-    return null;
+  public IBaseClass[] getMembers() {
+    return members.toArray(new IBaseClass[members.size()]);
   }
-
-
 }
