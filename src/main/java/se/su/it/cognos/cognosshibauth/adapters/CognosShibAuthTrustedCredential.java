f133521d4bb54152fa35c499208666e202a1c9ab
Joakim Lundin <jolu@it.su.se>
2011-04-19 09:17:03 +0200

    Refactor

--- src/main/java/se/su/it/cognos/cognosshibauth/adapters/CognosShibAuthTrustedCredential.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/adapters/CognosShibAuthTrustedCredential.java
@@ -1,13 +1,10 @@
-
 package se.su.it.cognos.cognosshibauth.adapters;
+
 import com.cognos.CAM_AAA.authentication.ITrustedCredential;
 
+public class CognosShibAuthTrustedCredential extends CognosShibAuthCredential implements ITrustedCredential {
 
-public class CognosShibAuthTrustedCredential extends CognosShibAuthCredential implements ITrustedCredential
-{
-
-  public CognosShibAuthTrustedCredential()
-  {
+  public CognosShibAuthTrustedCredential() {
     super();
   }
 }
