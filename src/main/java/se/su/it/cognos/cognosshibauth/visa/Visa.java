f70b3f0fd670e5062a9d6bf7f27c37377c21b30b
Joakim Lundin <jolu@it.su.se>
2011-04-20 13:30:02 +0200

    Using the correct classloader to get VisaValidator.

---
+++ src/main/java/se/su/it/cognos/cognosshibauth/visa/Visa.java
@@ -0,0 +1,159 @@
+package se.su.it.cognos.cognosshibauth.visa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.cognos.CAM_AAA.authentication.IAccount;
+import com.cognos.CAM_AAA.authentication.IBiBusHeader;
+import com.cognos.CAM_AAA.authentication.ICredential;
+import com.cognos.CAM_AAA.authentication.IGroup;
+import com.cognos.CAM_AAA.authentication.IRole;
+import com.cognos.CAM_AAA.authentication.ITrustedCredential;
+import com.cognos.CAM_AAA.authentication.IVisa;
+import com.cognos.CAM_AAA.authentication.SystemRecoverableException;
+import com.cognos.CAM_AAA.authentication.UnrecoverableException;
+import com.cognos.CAM_AAA.authentication.UserRecoverableException;
+import se.su.it.cognos.cognosshibauth.adapters.Credential;
+import se.su.it.cognos.cognosshibauth.adapters.TrustedCredential;
+import se.su.it.cognos.cognosshibauth.config.ConfigHandler;
+
+
+public class Visa implements IVisa {
+
+  private Logger LOG = Logger.getLogger(Visa.class.getName());
+
+  private List<IRole> roles;
+  private List<IGroup> groups;
+  private IAccount account;
+
+  private VisaValidator visaValidator = null;
+
+  private ConfigHandler configHandler = null;
+
+  public Visa(ConfigHandler configHandler) {
+    LOG.log(Level.FINEST, "Creating a Visa.");
+    roles = null;
+    groups = null;
+    this.configHandler = configHandler;
+  }
+
+  public void init(IAccount theAccount) throws UnrecoverableException {
+    account = theAccount;
+    
+    LOG.log(Level.FINEST, "Initing new account for '" + account.getUserName() + "'");
+
+    String visaValidatorClassName = configHandler.getVisaValidatorClass();
+    ClassLoader classLoader = Visa.class.getClassLoader();
+
+    try {
+      visaValidator = (VisaValidator) classLoader.loadClass(visaValidatorClassName).newInstance();
+    } catch (Exception e) {
+      LOG.log(Level.SEVERE, "Failed to load visa validator: " + e.getMessage() + " " + e.toString());
+      for(StackTraceElement ste : e.getStackTrace())
+        LOG.log(Level.SEVERE, "Failed to load visa validator: " + ste.getClassName() + " " + ste.getFileName() + " " + ste.getMethodName());
+      e.printStackTrace();
+      throw new UnrecoverableException("Failed to load visa validator.", e.getMessage());
+    }
+
+    visaValidator.init(account);
+  }
+
+  public void destroy() throws UnrecoverableException {
+    LOG.log(Level.FINEST, "Destroying Visa for '" + account.getUserName() + "'.");
+    roles = null;
+    groups = null;
+    account = null;
+    visaValidator.destroy();
+  }
+
+  public ITrustedCredential generateTrustedCredential(     // implementera ordentligt senare...
+          IBiBusHeader theAuthRequest) throws UserRecoverableException,
+          SystemRecoverableException, UnrecoverableException {
+
+    LOG.log(Level.FINEST, "Generating trusted credentials.");
+    boolean isValidCredentials = true;
+    String[] theUsername = null;
+    String[] thePassword = null;
+    theUsername = theAuthRequest.getCredentialValue("username");
+
+    if (theUsername == null && thePassword == null)
+       theUsername = new String[]{account.getUserName()};
+    else if (theUsername != null && theUsername.length == 1 && theUsername[0].equals(account.getUserName()))
+      isValidCredentials = false; // nånBraKoll(theUsername[0]);
+
+    if (!isValidCredentials) {
+      UserRecoverableException e = new UserRecoverableException(
+              "Please type your credentials for authentication.",
+              "The provided credentials are invalid.");
+      throw e;
+    }
+
+    TrustedCredential tc = new TrustedCredential();
+    tc.addCredentialValue("username", theUsername.toString());
+    return tc;
+  }
+
+  public ICredential generateCredential(IBiBusHeader theAuthRequest)
+          throws UserRecoverableException, SystemRecoverableException,
+          UnrecoverableException {
+    LOG.log(Level.FINEST, "Generating credentials.");
+    boolean validCredential = true; //nånBraKoll(account.getUserName());
+    if(! validCredential){
+      UnrecoverableException e = new UnrecoverableException(
+              "Could not generate credentials for the user.",
+              "Visa contains invalid credentials.");
+      throw e;
+    }
+    else {
+      Credential credentials = new Credential();
+      credentials.addCredentialValue("username", account.getUserName());
+      return credentials;
+    }
+  }
+
+  public boolean isValid() {
+    LOG.log(Level.FINEST, "Checking isValid.");
+    return visaValidator.isValid();
+  }
+
+  public IAccount getAccount() {
+    LOG.log(Level.FINEST, "Getting account for '" + account.getUserName() + "'.");
+    return account;
+  }
+
+
+  public void addGroup(IGroup theGroup) {
+    LOG.log(Level.FINEST, "Adding group to Visa for '" + account.getUserName() + "'.");
+    if (groups == null)
+      groups = new ArrayList<IGroup>();
+    groups.add(theGroup);
+  }
+
+
+  public IGroup[] getGroups() {
+    LOG.log(Level.FINEST, "Getting groups from Visa for '" + account.getUserName() + "'.");
+    if (groups != null)
+      return groups.toArray(new IGroup[groups.size()]);
+    return null;
+  }
+
+
+  public void addRole(IRole theRole) {
+    LOG.log(Level.FINEST, "Adding role to Visa for '" + account.getUserName() + "'.");
+    if (roles == null)
+      roles = new ArrayList<IRole>();
+    roles.add(theRole);
+  }
+
+
+
+  public IRole[] getRoles() {
+    LOG.log(Level.FINEST, "Getting roles from Visa for '" + account.getUserName() + "'.");
+    if (roles != null)
+      return roles.toArray(new IRole[roles.size()]);
+    return null;
+  }
+
+}
