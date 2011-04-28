45116ef6612204c33fde8621c61426912145cad5
Joakim Lundin <jolu@it.su.se>
2011-04-28 11:41:33 +0200

    Added the ability to have children in folders.

--- src/main/java/se/su/it/cognos/cognosshibauth/adapters/NamespaceFolder.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/adapters/NamespaceFolder.java
@@ -1,16 +1,23 @@
 package se.su.it.cognos.cognosshibauth.adapters;
 
 import com.cognos.CAM_AAA.authentication.INamespaceFolder;
+import com.cognos.CAM_AAA.authentication.IUiClass;
 import se.su.it.cognos.cognosshibauth.config.ConfigHandler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class NamespaceFolder extends UiClass implements INamespaceFolder {
 
   private Locale defaultLocale = null;
 
+  private List<IUiClass> children = null;
+
   public NamespaceFolder(String theSearchPath, String name) {
     super(theSearchPath + ":" + PREFIX_FOLDER + name);
+
+    children = new ArrayList<IUiClass>();
 
     ConfigHandler configHandler = ConfigHandler.instance();
     defaultLocale = configHandler.getContentLocale();
@@ -19,6 +26,18 @@ public class NamespaceFolder extends UiClass implements INamespaceFolder {
   }
 
   public boolean getHasChildren() {
-    return true;
+    return children.size() > 0;
+  }
+
+  public void addDescription(String description) {
+    addDescription(defaultLocale, description);
+  }
+
+  public void addChild(IUiClass iUiClass) {
+    children.add(iUiClass);
+  }
+
+  public List<IUiClass> getChildren() {
+    return children;
   }
 }
