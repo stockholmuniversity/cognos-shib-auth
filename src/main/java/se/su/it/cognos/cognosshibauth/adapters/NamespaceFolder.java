f7401efcfe760e03a4ad47809811f48f19aade57
Joakim Lundin <jolu@it.su.se>
2011-04-28 07:03:00 +0200

    Set folder prefix in folder class.

--- src/main/java/se/su/it/cognos/cognosshibauth/adapters/NamespaceFolder.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/adapters/NamespaceFolder.java
@@ -10,7 +10,7 @@ public class NamespaceFolder extends UiClass implements INamespaceFolder {
   private Locale defaultLocale = null;
 
   public NamespaceFolder(String theSearchPath, String name) {
-    super(theSearchPath);
+    super(theSearchPath + ":f:" + name);
 
     ConfigHandler configHandler = ConfigHandler.instance();
     defaultLocale = configHandler.getContentLocale();
