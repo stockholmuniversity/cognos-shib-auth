f4804a7cc39f708889334f7ca52732a3e45583c5
Joakim Lundin <jolu@it.su.se>
2011-05-10 08:09:21 +0200

    Removed unused variable.

--- src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuthBase.java
+++ src/main/java/se/su/it/cognos/cognosshibauth/CognosShibAuthBase.java
@@ -92,7 +92,6 @@ public class CognosShibAuthBase extends CognosShibAuthNamespace implements IName
             }
             else if (isUser(objectID) && filter == null) {
               String uid = camIdToName(objectID);
-              SUKAT sukat = SUKAT.newInstance(configHandler.getStringEntry("adapters.url"));
               Account account = Account.findByUid(namespaceId, uid);
               result.addObject(account);
             }
