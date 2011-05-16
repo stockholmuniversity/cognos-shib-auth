83239b62b2568ccc1c6ba747e19dd4aac8a83e5c
Joakim Lundin <jolu@it.su.se>
2011-05-16 08:58:29 +0200

    imports

--- src/test/groovy/se/su/it/cognos/cognosshibauth/CognosShibAPTest.groovy
+++ src/test/groovy/se/su/it/cognos/cognosshibauth/CognosShibAPTest.groovy
@@ -6,6 +6,11 @@ import org.junit.runner.RunWith
 import static org.junit.Assert.assertEquals
 import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.when
+import com.cognos.CAM_AAA.authentication.IBiBusHeader
+import org.powermock.core.classloader.annotations.PrepareForTest
+import static org.powermock.api.mockito.PowerMockito.spy
+import static org.powermock.api.mockito.PowerMockito.doReturn
+import com.cognos.CAM_AAA.authentication.IBiBusHeader2
 
 @RunWith(org.powermock.modules.junit4.PowerMockRunner.class)
 public class CognosShibAPTest {
