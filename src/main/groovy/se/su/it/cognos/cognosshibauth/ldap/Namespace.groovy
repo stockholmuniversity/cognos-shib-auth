feb23f25f8510147a93a49fddac42da5bb5b0de3
Henrik Dryselius <henrik.dryselius@it.su.se>
2011-05-16 09:03:41 +0200

    code comments

--- src/main/groovy/se/su/it/cognos/cognosshibauth/ldap/Namespace.groovy
+++ src/main/groovy/se/su/it/cognos/cognosshibauth/ldap/Namespace.groovy
@@ -8,7 +8,10 @@ class Namespace extends UiClass implements INamespace {
 
   private String[] capabilities;
   private String namespaceFormat;
-
+  /**
+   * Constructs a NameSpace instance
+   *
+   */
   public Namespace() {
     super(null);
 
@@ -22,7 +25,11 @@ class Namespace extends UiClass implements INamespace {
 
     namespaceFormat = "http://developer.cognos.com/schemas/CAM/AAANamespaceFormat/2/";
   }
-
+  /**
+   * Constructs a NameSpace instance based on an objectId
+   * @param theObjectID
+   *
+   */
   public Namespace(String theObjectID) {
     super(theObjectID);
     capabilities = new String[6];
@@ -34,29 +41,29 @@ class Namespace extends UiClass implements INamespace {
     capabilities[5] = CapabilityEndsWith;
     namespaceFormat = "http://developer.cognos.com/schemas/CAM/AAANamespaceFormat/2/";
   }
-
+  @Override
   public void init(INamespaceConfiguration theNamespaceConfiguration)
           throws UnrecoverableException {
     setObjectID(theNamespaceConfiguration.getID());
     addName(theNamespaceConfiguration.getServerLocale(),
             theNamespaceConfiguration.getDisplayName());
   }
-
+  @Override
   public void destroy() {
   }
-
+  @Override
   public String getNamespaceFormat() {
     return namespaceFormat;
   }
-
+  @Override
   public void setNamespaceFormat(String theNamespaceFormat) {
     namespaceFormat = theNamespaceFormat;
   }
-
+  @Override
   public String[] getCapabilities() {
     return capabilities;
   }
-
+  @Override
   public boolean getHasChildren() {
     return true;
   }
