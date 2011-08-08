00c22273c9844f96c5afd1fbb6590148cbc46c98
Joakim Lundin <jolu@it.su.se>
2011-08-08 12:33:45 +0200

    Test for Group.

---
+++ src/test/groovy/se/su/it/cognos/cognosshibauth/ldap/GroupTest.groovy
@@ -0,0 +1,28 @@
+package se.su.it.cognos.cognosshibauth.ldap
+
+import org.junit.Test
+import se.su.it.cognos.cognosshibauth.ldap.schema.SuPerson
+import se.su.it.cognos.cognosshibauth.ldap.schema.GroupOfUniqueNames
+
+class GroupTest {
+
+  @Test
+  void testFindByMember() {
+    SuPerson mockSuPerson = new SuPerson()
+    mockSuPerson.metaClass.getDn = { "uid=test1,dc=it,dc=su,dc=se" }
+
+    GroupOfUniqueNames mockGroupOfUniqueNames = new GroupOfUniqueNames()
+    mockGroupOfUniqueNames.metaClass.getDn = { "cn=group1,dc=it,dc=su,dc=se" }
+
+    Account.metaClass.static.createTestAccount << { new Account(mockSuPerson) }
+
+    GroupOfUniqueNames.metaClass.'static'.findAllByUniqueMember = { String memberDn ->
+      [mockGroupOfUniqueNames]
+    }
+
+    def account = Account.createTestAccount()
+    def groups = Group.findByMember(account)
+
+    assert groups.contains(new Group(mockGroupOfUniqueNames))
+  }
+}
