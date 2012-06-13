83a465c4cf902dad8aa22d2684008034db4ad860
Joakim Lundin <joakim.lundin@su.se>
2012-06-13 14:28:11 +0200

    Added tests for grenerateCredentials

--- src/test/groovy/se/su/it/cognos/cognosshibauth/visa/VisaSpock.groovy
+++ src/test/groovy/se/su/it/cognos/cognosshibauth/visa/VisaSpock.groovy
@@ -9,6 +9,10 @@ import com.cognos.CAM_AAA.authentication.IAccount
 import org.spockframework.runtime.Sputnik
 import org.junit.runner.RunWith
 import spock.lang.Specification
+import com.cognos.CAM_AAA.authentication.IBiBusHeader
+import spock.lang.Unroll
+import se.su.it.cognos.cognosshibauth.ldap.Account
+import com.cognos.CAM_AAA.authentication.UnrecoverableException
 
 @RunWith(Sputnik)
 public class VisaSpock extends Specification {
@@ -30,5 +34,40 @@ public class VisaSpock extends Specification {
 
     then:
     assertTrue(obj instanceof DummyVisaValidator)
+  }
+
+  def "Test generateCredentials no exception"() {
+    setup:
+    Visa target = new Visa(null)
+
+    IAccount account = [ getUserName: { 'jolu' } ] as IAccount
+    VisaValidator visaValidator = [ isValid: { true }] as VisaValidator
+
+    Visa.metaClass.setProperty(Visa, target, 'visaValidator', visaValidator, false, true)
+    Visa.metaClass.setProperty(Visa, target, 'account', account, false, true)
+
+    when:
+    def result = target.generateCredential(null)
+
+    then:
+    notThrown(UnrecoverableException)
+    result.getCredentialValue('username').first() == 'jolu'
+  }
+
+  def "Test generateCredentials throws exception for invalid visa"() {
+    setup:
+    Visa target = new Visa(null)
+
+    IAccount account = [ getUserName: { 'jolu' } ] as IAccount
+    VisaValidator visaValidator = [ isValid: { false }] as VisaValidator
+
+    Visa.metaClass.setProperty(Visa, target, 'visaValidator', visaValidator, false, true)
+    Visa.metaClass.setProperty(Visa, target, 'account', account, false, true)
+
+    when:
+    def result = target.generateCredential(null)
+
+    then:
+    thrown(UnrecoverableException)
   }
 }
