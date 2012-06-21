ec88ec9dccf0b1e0353e07f8fd445c9b2ecb16f7
Joakim Lundin <joakim.lundin@su.se>
2012-06-21 14:56:59 +0200

    Made serializable.

--- src/main/groovy/se/su/it/cognos/cognosshibauth/ldap/schema/GroupOfUniqueNames.groovy
+++ src/main/groovy/se/su/it/cognos/cognosshibauth/ldap/schema/GroupOfUniqueNames.groovy
@@ -11,7 +11,9 @@ import se.su.it.cognos.cognosshibauth.memcached.Cache
  */
 
 @GldapoSchemaFilter("(objectClass=groupOfUniqueNames)")
-class GroupOfUniqueNames extends SchemaBase {
+class GroupOfUniqueNames extends SchemaBase  implements Serializable {
+
+  private static final long serialVersionUID = 4481281134744979887L ;
 
   @GldapoNamingAttribute
   String cn
@@ -20,5 +22,13 @@ class GroupOfUniqueNames extends SchemaBase {
 
   static Collection<GroupOfUniqueNames> findAllByUniqueMember(String memberDn) {
     findAll(filter:"(uniqueMember=$memberDn)")
+  }
+
+  private void writeObject(ObjectOutputStream s) {
+    s.defaultWriteObject()
+  }
+
+  private void readObject(ObjectInputStream s) {
+    s.defaultReadObject()
   }
 }
